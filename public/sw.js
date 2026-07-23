beefd8bbb47741e4403f2041651e9f448c834bed
RochTools <hubproapk@gmail.com>
2026-07-24 03:01:35 +0500

    Upgrade service worker to CACHE_NAME v2
    Updated service worker to use a new cache version and improved offline handling.

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,31 @@
-const CACHE_NAME = 'profit-tracker-v1';
+const CACHE_NAME = 'profit-tracker-v2';
+const OFFLINE_URL = '/index.html';
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(['/', '/index.html']);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.add(OFFLINE_URL))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match(OFFLINE_URL)))
   );
 });
